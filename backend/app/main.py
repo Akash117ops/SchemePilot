f8abe65107d9d46cb541aa5634acbb36fffff623
Akash <akashmanavarthi@gmail.com>
2026-07-26 08:39:48 +0530

    Setup database and create User model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
+from app.database import Base, engine
+from app.models import User
+
+print("Database URL:", engine.url)
+print("Registered tables:", Base.metadata.tables.keys())
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
 
 @app.get("/")
 def home():
-    return {"message": "Hello,there!"}
+    return {"message": "Welcome to SchemePilot API!"}
